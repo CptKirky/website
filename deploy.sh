43be288c23fa8000418e5efce7c443a72b8e065e
Michael Gale <m.gale@warwick.ac.uk>
2020-04-14 17:25:45 +0100

    Add deployment script for release pipeline

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+set -e
+
+SB_UTIL=$(System.ArtifactsDirectory)/sitebuilder-util
+SB_ROOT=/fac/sci/dcs/teaching/material/cs407
+
+chmod +x $SB_UTIL
+
+$SB_UTIL edit --page=$SB_ROOT --file=material.html
+$SB_UTIL edit --page=$SB_ROOT/components/specification/ --file=specification.html
+$SB_UTIL edit --page=$SB_ROOT/components/progress/ --file=progress.html
+$SB_UTIL edit --page=$SB_ROOT/components/demo/ --file=final.html
+$SB_UTIL edit --page=$SB_ROOT/suggestions/ --file=suggestions.html
+$SB_UTIL edit --page=$SB_ROOT/assessment/ --file=assessment.html
